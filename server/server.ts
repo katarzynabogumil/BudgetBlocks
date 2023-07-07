9bbbf022aaa9200eb76dab15eaa0c7e98b8c6618
Katarzyna Bogumil <katarzyna.bogumil@gmail.com>
2023-07-07 22:04:39 +0200

    fix: final flyctl setup

--- server/server.ts
+++ server/server.ts
@@ -3,5 +3,5 @@ import { app } from './index';
 const PORT = parseInt(process.env.PORT || '', 10);
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at ${PORT}`);
 });
