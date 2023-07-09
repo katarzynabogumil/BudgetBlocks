6fcffcae4577a7e6d07200e75c88da7a19733a11
Katarzyna Bogumil <katarzyna.bogumil@gmail.com>
2023-07-09 12:04:57 +0200

    fix: loading project dashboard display

--- server/server.ts
+++ server/server.ts
@@ -5,5 +5,5 @@ const log = debug('app:log');
 const PORT = parseInt(process.env.PORT || '', 10);
 
 app.listen(PORT, () => {
-  log(`DEBUG Server running at ${PORT}`);
+  log(`Server running at ${PORT}`);
 });
